5478652175bdf8ca2c2c4ac5e1795a4d25b68591
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-11-19 07:23:31 -0500

    style: change log format

--- log.go
+++ log.go
@@ -9,6 +9,13 @@ import (
 )
 
 func warn(text string) {
-	fmt.Printf("%v%v%v%s\n", color.HiBlackString("["), color.YellowString("-"), color.HiBlackString("] "), text)
+	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.YellowString("-"), color.HiBlackString(") "), text)
 }
 
+func info(text string) {
+	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.GreenString("+"), color.HiBlackString(") "), text)
+}
+
+func logerror(text string) {
+	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.RedString("!"), color.HiBlackString(") "), text)
+}
