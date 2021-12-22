05980f35c4b36bc58d86f4d98487598e5440d366
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2021-12-22 00:01:35 -0400

    fix(log): use sprint without newline

--- log/log.go
+++ log/log.go
@@ -9,13 +9,13 @@ import (
 )
 
 func Warn(text ...interface{}) {
-	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.YellowString("-"), color.HiBlackString(") "), fmt.Sprintln(text...))
+	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.YellowString("-"), color.HiBlackString(") "), fmt.Sprint(text...))
 }
 
 func Info(text ...interface{}) {
-	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.GreenString("+"), color.HiBlackString(") "), fmt.Sprintln(text...))
+	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.GreenString("+"), color.HiBlackString(") "), fmt.Sprint(text...))
 }
 
 func Error(text ...interface{}) {
-	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.RedString("!"), color.HiBlackString(") "), fmt.Sprintln(text...))
+	fmt.Printf("%v%v%v%s\n", color.HiBlackString("("), color.RedString("!"), color.HiBlackString(") "), fmt.Sprint(text...))
 }
